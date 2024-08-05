f663f32b386de71a49f3e22a349ebc5a11accc7a
Rafael Araujo Santos <rafael.araujo03.07.07@gmail.com>
2024-08-05 15:12:23 -0300

    comentarios adicionados ao código

--- piscarLed/piscarLed.ino
+++ piscarLed/piscarLed.ino
@@ -1,10 +1,16 @@
+//Programa que envia sinal alto para a saida 7
+//aguarde 1 segundo e repita novamente
 void setup() {
+  //definir porta 7 como saida
   pinMode (7, OUTPUT);
 }
 
+//definir a parte do código que se repete
 void loop() {
+  //envia sinal alto
   digitalWrite(7, HIGH);
   delay (1000);
+  //envia sinal baixo
   digitalWrite(7, LOW);
   delay(1000);
 }
